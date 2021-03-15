3358e0d1e13011cef36f969d402cc40bbe0b8492
jeganjp <jeganjp1999@gmail.com>
2021-03-15 13:41:31 +0530

    changed browser router to hashrouter

--- src/Level1.js
+++ src/Level1.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import ingamebg from './images/6335.jpg';
+import Level1bg from './images/Level1bg.jpg';
 import happy from './images/happy emoji.jfif';
 import sad from './images/sad emoji.jfif';
 import angry from './images/angry emoji.png';
@@ -13,34 +13,33 @@ const Level1 = () => {
         justifyContents:'center',
         justifyItems:'center',
         alignItems:'center',
-        background:'yellow'
-        // backgroundImage:`url(${ingamebg})`,
-        // backgroundSize:'cover'
+        backgroundImage:`url(${Level1bg})`,
+        backgroundSize:'cover'
     }}>
                 <h1 style={{color:'Background',fontWeight:'bolder'}}>Guess the Emoji</h1>
                 <Timer/>
-                <div style={{alignSelf:'baseline'}}>
+                <div style={{padding:30,border:'solid'}}>
                 <ul>
                 <li type='1' style={{}}><img style={{}} src={happy} alt='happy' height={80} width={80}/>
-                <input type='radio' value='happy' style={{height:20,width:20}} /><span style={{fontSize:30}}>Happy</span>
-                <input type='radio' value='sad' style={{height:20,width:20}}/><span style={{fontSize:30}}>Sad</span>
-                <input type='radio' value='angry' style={{height:20,width:20}}/><span style={{fontSize:30}}>Angry</span>    
+                <input type='radio' value='happy' style={{height:20,width:20,margin:20}} /><span style={{fontSize:30}}>Happy</span>
+                <input type='radio' value='sad' style={{height:20,width:20,margin:20}}/><span style={{fontSize:30}}>Sad</span>
+                <input type='radio' value='angry' style={{height:20,width:20,margin:20}}/><span style={{fontSize:30}}>Angry</span>    
                 </li>
                 <li type='1'>
                 <img src={sad} alt='sad' height={80} width={80}/>
-                <input type='radio' value='happy' style={{height:20,width:20}} /><span style={{fontSize:30}}>Happy</span>
-                <input type='radio' value='sad' style={{height:20,width:20}}/><span style={{fontSize:30}}>Sad</span>
-                <input type='radio' value='angry' style={{height:20,width:20}}/><span style={{fontSize:30}}>Angry</span>    
+                <input type='radio' value='happy' style={{height:20,width:20,margin:20}} /><span style={{fontSize:30}}>Happy</span>
+                <input type='radio' value='sad' style={{height:20,width:20,margin:20}}/><span style={{fontSize:30}}>Sad</span>
+                <input type='radio' value='angry' style={{height:20,width:20,margin:20}}/><span style={{fontSize:30}}>Angry</span>    
                 </li>
                 <li type='1'>
                 <img style={{background:'white'}} src={angry} alt='angry' height={80} width={80}/>
-                <input type='radio' value='happy' style={{height:20,width:20}} /><span style={{fontSize:30}}>Happy</span>
-                <input type='radio' value='sad' style={{height:20,width:20}}/><span style={{fontSize:30}}>Sad</span>
-                <input type='radio' value='angry' style={{height:20,width:20}}/><span style={{fontSize:30}}>Angry</span>
+                <input type='radio' value='happy' style={{height:20,width:20,margin:20}} /><span style={{fontSize:30}}>Happy</span>
+                <input type='radio' value='sad' style={{height:20,width:20,margin:20}}/><span style={{fontSize:30}}>Sad</span>
+                <input type='radio' value='angry' style={{height:20,width:20,margin:20}}/><span style={{fontSize:30}}>Angry</span>
                 </li>
                 </ul>    
                 </div>
-                <button type='submit'><Link to='Level2'>Submit</Link></button>
+                <button type='submit'><Link to='Level2'><span style={{color:'white'}}>Submit</span></Link></button>
         </div>
 
     )
--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import {BrowserRouter} from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
   <React.StrictMode>
     <App/>
   </React.StrictMode>
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById('root')
 );
 
